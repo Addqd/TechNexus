b7ff430e7e342f4ac3f7a3fceb4e02a180b65c9d
Addqd <licnayavesh@yandex.ru>
2025-03-29 17:13:21 +0300

    Initial markup of profile component

--- tech-nexus-frontend/src/UserProfile/UserProfile.jsx
+++ tech-nexus-frontend/src/UserProfile/UserProfile.jsx
@@ -23,19 +23,73 @@ export default function UserProfile () {
                     Ваш бренд
                 </div>
                 <div className={styles.section}>
-                    Баланс
+                    Способ оплаты
                 </div>
             </div>
 
             {/* Components corresponding to choises */}
 
+            {/* Profile */}
+
             <div className={styles.profileWrapper}>
                 <div className={styles.profileContent}>
                     <img src="/images/testImage.jpg" alt="Изображение профиля" />
                     <span>Username</span>
-                    
+                    <span>Баланс 100</span>
+                    <span>Продавец ли?</span>
+                    <span>Редактировать профиль</span>
                 </div>
             </div>
+
+            {/* Orders */}
+
+            <div className={styles.ordersWrapper}>
+                <div className={styles.ordersContent}>
+                    <span>Product card goes here</span>
+                    <span>Status of the purchase</span>
+                    <span>Approximate date of arrival</span>
+                </div>
+            </div>
+
+            {/* History */}
+
+            <div className={styles.historyWrapper}>
+                <div className={styles.historyContent}>
+                    <span>Product card goes here</span>
+                    <span>Rewiev, if never reviewed: Provide an option to do so</span>
+                </div>
+            </div>
+
+            {/* Favorites */}
+
+            <div className={styles.favoritesWrapper}>
+                <div className={styles.favoritesContent}>
+                    <span>Product card goes here</span>
+                    <span>Is in stock, if true - it's ammount</span>
+                </div>
+            </div>
+
+            {/* Brand */}
+
+            <div className={styles.brandWrapper}>
+                <div className={styles.brandContent}>
+                    <span>Create a brand if not registered</span>
+                    <img src="/images/testImage.jpg" alt="Изображение бренда" />
+                    <span>Brand name</span>
+                    <span>Отзывы клиентов</span>
+                    <span>Создать товар</span>
+                    <span>Product card goes here, all brand's products</span>
+                </div>
+            </div>
+
+            {/* Payment method */}
+            
+            <div className={styles.paymentWrapper}>
+                <div className={styles.paymentContent}>
+                    <span>Cool looking box like banking card, with registration</span>
+                </div>
+            </div>
+
         </>
     );   
 }
